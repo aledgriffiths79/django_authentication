f88681faf6aa1f971d65314919f85bf7750fb59d
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-03-04 13:57:46 +0000

    The logout functionality:Logs a user out of the application:By using Djangos logout function

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 # Create your views here.
 
@@ -7,3 +8,9 @@ from django.shortcuts import render
 def index(request):
   """Return the index.html file and pass in the request and the name of the html file that we want to serve"""
   return render(request, 'index.html')
+
+def logout(request):
+  """Log the user out"""
+  auth.logout(request)
+  return redirect(reverse('index'))
+  # reverse allows us to pass the name of a url instead of a name of a view
