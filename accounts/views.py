9d6e208f3db4b1e391db37c69df4e949fefab2e1
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-03-05 13:14:55 +0000

    A login template and view: Will allow us to create a form once they're in place:By creating a Django view and template

--- accounts/views.py
+++ accounts/views.py
@@ -15,3 +15,7 @@ def logout(request):
   messages.success(request, "You have successfully been logged out!")
   return redirect(reverse('index'))
   # reverse allows us to pass the name of a url instead of a name of a view
+
+def login(request):
+  """Return a login page"""
+  return render(request, 'login.html')
