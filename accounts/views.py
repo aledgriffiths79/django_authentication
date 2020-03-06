cd2d1e676ae54a0de8b76526960215968f2c507e
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-03-06 18:48:23 +0000

    The login_required decorator:Allows us to redirect users depending on whether or not they're authenticated: By importing the login_required decorator and adding it to a view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
+
 
 # Create your views here.
 
@@ -10,6 +12,7 @@ def index(request):
   """Return the index.html file and pass in the request and the name of the html file that we want to serve"""
   return render(request, 'index.html')
 
+@login_required
 def logout(request):
   """Log the user out"""
   auth.logout(request)
@@ -19,6 +22,8 @@ def logout(request):
 
 def login(request):
   """Return a login page"""
+  if request.user.is_authenticated:
+    return redirect(reverse('index'))
   if request.method == 'POST':
     login_form = UserLoginForm(request.POST)
     # retrieve the username from the POST dictionary, same for password. we will use the password key to retieve the password from the dictionary and this will authenticate the user
@@ -28,6 +33,10 @@ def login(request):
       if user:
         auth.login(user=user, request=request)
         messages.success(request, "You have successfully logged in!")
+        # after a user has successfully logged in what we want to do is we want
+        # to redirect them to a specific page just so that they're not redirected back to
+        # the login page
+        return redirect(reverse('index'))
       else:
         login_form.add_error(None, 'Your username or password is incorrect!')
   else:
