4dc36dc852c67a30479a933a57c5554a266b0c80
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-03-09 18:44:21 +0000

    UserCreationForm:Allows us to extend it so we dont need to create an entire form from scratch:By importing it from the auth app and creating a new form class that inherits from it

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from django.contrib.auth.models import User
+# usercreationform is a base form that django provides us and it will give us user names and emails
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
+
+# the forms section for registration requires us to store some info in the database about the user. To achieve this we're going to from django.contrib.auth.models import user. This will import the user model provided by Django
 # create a form object
 
 class UserLoginForm(forms.Form):
@@ -9,3 +15,17 @@ class UserLoginForm(forms.Form):
   # constructor
   username = forms.CharField()
   password = forms.CharField(widget=forms.PasswordInput)
+
+# this form will inherit from usercreationform this will give us the username and email fields
+class UserRegistrationForm(UserCreationForm):
+  """Form used to register a new user"""
+
+  password = forms.CharField(widget=forms.PasswordInput)
+  password2 = forms.CharField(
+      label="Password Confirmation", 
+      widget=forms.PasswordInput)
+
+  # inner class Meta
+  class Meta:
+    model = User
+    fields = ['email', 'username', 'password1', 'password2']
